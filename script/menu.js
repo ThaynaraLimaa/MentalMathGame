879a12192b78048ef7444057fadaf47d90522bae
ThaynaraLimaa <thaynaralimaa61@gmail.com>
2022-12-06 14:41:23 -0300

    Save theme preferences in local storage

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,12 @@
+//load the previously theme choice if there is one. 
+window.addEventListener('load', () => {
+    if(localStorage.getItem('themeChoice') == 1) {
+        html.classList.add('dark-mode')
+    }
+} )
+
+
+
 // Show and hide menu
 let hamburguerBtn = document.querySelector('#hamburguerBtn-js')
 
@@ -6,8 +15,15 @@ hamburguerBtn.addEventListener('click', () =>{
 })
 
 // Switch themes
+
 const chkBtn = document.querySelector('#inputCheckbox')
 const html = document.querySelector('html')
 chkBtn.addEventListener('change', () => {
     html.classList.toggle('dark-mode')
+
+    if(html.classList.contains('dark-mode')) {
+        localStorage.setItem("themeChoice", 1)
+    } else {
+        localStorage.setItem("themeChoice", 2)
+    }
 })
